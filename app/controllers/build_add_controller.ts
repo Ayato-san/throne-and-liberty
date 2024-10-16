0bb3863f50df534b7e737ab15e0760c645fa02c2
Ayato <ayatosan.pro@gmail.com>
2024-10-16 19:45:47 +0200

    feat: Update BuildAddController and BuildsRepository
    - Update BuildAddController to include a new schema for request validation
    - Update BuildsRepository to implement the create method

--- app/controllers/build_add_controller.ts
+++ app/controllers/build_add_controller.ts
@@ -4,6 +4,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 import PlayerBuild from '#models/player_build'
 import BuildsRepository from '../repositories/builds_repository.js'
 import ClassesRepository from '../repositories/classes_repository.js'
+import vine from '@vinejs/vine'
 
 @inject()
 export default class BuildAddController {
@@ -12,13 +13,30 @@ export default class BuildAddController {
     private classesRepository: ClassesRepository
   ) {}
 
+  /** The schema to validate the request */
+  static schema = vine.object({
+    class: vine.string().uuid(),
+    scale: vine.enum(['small', 'large']).optional().requiredWhen('type', '=', 'PvP'),
+    type: vine.enum(['PvP', 'PvE']),
+  })
+
   async handle({ inertia }: HttpContext) {
     const classes = await this.classesRepository.all()
 
-    const scales: PlayerBuild['scale'][] = [null, 'small', 'large']
+    const scales: Exclude<PlayerBuild['scale'], null>[] = ['small', 'large']
 
     const types: PlayerBuild['type'][] = ['PvP', 'PvE']
 
     return inertia.render('builds/add', { classes, scales, types })
+  }
+
+  async execute({ request, response }: HttpContext) {
+    const data = request.only(['class', 'scale', 'type'])
+
+    const validateData = await vine.validate({ schema: BuildAddController.schema, data })
+
+    await this.buildRepository.create(validateData)
+
+    response.redirect().back()
   }
 }
